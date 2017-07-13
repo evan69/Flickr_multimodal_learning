fba5de87e034e32a6c7a02331c1d474eb8f1b7a0
evan69 <evan9669@sina.com>
2017-07-13 15:59:41 +0800

    get tags for supervised tags

--- tags_crawl.py
+++ tags_crawl.py
@@ -35,17 +35,9 @@ print LO_DATE
 root_dir = '../data/unsupervised/'
 cnt = 0
 
-def judgeAdj(tag):
-# judge whether the word is adj
-    if tag.find(' ') != -1:
-        return False
-    text = nltk.word_tokenize(tag)
-    res = nltk.pos_tag(text)
-    #print res
-    return res[0][1] == 'JJ'
-
 def getTagsByPhoto(photo_id):
     global cnt
+    tags = []
     while True:
         try:
             res = flickr.tags.getListPhoto(api_key=api_key,photo_id=photo_id)
@@ -56,18 +48,25 @@ def getTagsByPhoto(photo_id):
         except Exception,ex:
             print 'error in getTagsByPhoto , now try again :',photo_id
             print Exception,':',ex
-            if ex.code == 1:
-                break
-            continue
+            if Exception == requests.ConnectionError:
+                continue
         break
     return tags
 
 def crawl(dir,name):
+    global cnt
     image_set = set()
+    '''
+    try:
+        os.remove(dir + 'tags_data.txt')
+    except Exception,ex:
+        print ex
+    '''
     try:
         file_in = open(dir + 'tags_data.txt','r')
         lines = file_in.readlines()
         image_set = set([line.split(' ')[0] for line in lines])
+        cnt += len(image_set)
         file_in.close()
     except Exception,ex:
         print ex
@@ -82,39 +81,74 @@ def crawl(dir,name):
         if res[0] in image_set:
             continue
         tags = getTagsByPhoto(res[0])
-        if len(tags) == 0:
-            continue
-        file_out.write(res[0])
+        out = ''
+        # file_out.write(res[0])
         for tag in tags:
             t = tag.get('raw')
             if ' ' not in t:
                 # print t.decode('utf8')
-                file_out.write(' ' + t)
-        file_out.write(' \n')
+                # file_out.write(' ' + t)
+                out += (' ' + t)
+        if len(out) < 1:
+            continue
+        out = res[0] + out + '\n'
+        file_out.write(out)
         
     
 class MyThread(threading.Thread):
-    def __init__(self,id):
+    def __init__(self,id,isSup,all_tags):
         super(MyThread, self).__init__()
         self.id = id
+        self.isSup = isSup
+        self.all_tags = all_tags
     def run(self):
-        init = LO_DATE + datetime.timedelta(days = 30 * self.id)
-        for i in range(30):
-            dir = '../data/unsupervised/' + init.strftime("%y_%m_%d") + '/'
-            crawl(dir,self.getName())
-            init += datetime.timedelta(days = 1)
+        if self.isSup:
+            my_tags = [self.all_tags[i] for i in range(len(self.all_tags)) if i % THREAD_NUM == self.id]
+            for tag in my_tags:
+                dir = '../data/' + tag + '/'
+                try:
+                    crawl(dir,self.getName())
+                    print 'success'
+                except Exception,ex:
+                    print 'error in crawl'
+                    print ex
+        else:
+            init = LO_DATE + datetime.timedelta(days = 30 * self.id)
+            for i in range(30):
+                dir = '../data/unsupervised/' + init.strftime("%y_%m_%d") + '/'
+                try:
+                    crawl(dir,self.getName())
+                    init += datetime.timedelta(days = 1)
+                except Exception,ex:
+                    print 'error in crawl'
+                    print ex
 
 def main():
 
     reload(sys)
     sys.setdefaultencoding('utf-8')
     
+    isSup = True
+    if sys.argv[1] == '-s':
+        isSup = True
+        tag_file = open('pic_tag_cnt_new.txt','r')
+        all_tags = tag_file.readlines()
+        all_tags = [line.split(' ')[0] for line in all_tags]
+        tag_file.close()
+    else:
+        if sys.argv[1] == '-u':
+            isSup = False
+            all_tags = []
+        else:
+            print 'error in parameters'
+            return
+
     thread_list = []
     
     start = time.clock()
 
     for i in xrange(THREAD_NUM):
-        t = MyThread(int(i))
+        t = MyThread(int(i),isSup,all_tags)
         t.setDaemon(True)
         t.setName('thread-' + str(i))
         t.start()
